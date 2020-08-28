0ff53fbbf37a6105b8e1327b7dc07a43ceafb24a
le314u <lucsmfpiu@gmail.com>
2020-08-28 14:51:13 -0300

    mudando Bd Mint

---
+++ gsettingsMint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#Area de trabalho
+gsettings set org.cinnamon.theme name 'Mint-Y-Dark-Pink'
+#Ponteiro
+gsettings set org.cinnamon.desktop.interface cursor-theme 'DMZ-Black'
+#Controles
+gsettings set org.cinnamon.desktop.interface gtk-theme 'Mint-Y-Dark-Pink'
+#Icones
+gsettings set org.cinnamon.desktop.interface icon-theme 'Mint-Y-Dark-Pink'
+#Walpaper
+gsettings set org.cinnamon.desktop.background picture-uri 'file:///home/guest/Imagens/Captura%20de%20tela%20de%202020-08-27%2011-56-53.png'
+#Sons
+gsettings set org.cinnamon.sounds map-file '/usr/share/mint-artwork/sounds/map.oga'
+#
+gsettings set org.cinnamon.sounds unplug-file '/usr/share/mint-artwork/sounds/unplug.oga'
+#
+gsettings set org.cinnamon.sounds logout-file '/usr/share/mint-artwork/sounds/unplug.oga'
+#
+gsettings set org.cinnamon.sounds login-file '/usr/share/mint-artwork/sounds/unplug.oga'
+#
+gsettings set org.cinnamon.sounds plug-file '/usr/share/mint-artwork/sounds/unplug.oga'
+#
+gsettings set org.cinnamon.sounds close-file '/usr/share/mint-artwork/sounds/unplug.oga'
+#
+gsettings set org.cinnamon.sounds maximize-file '/usr/share/mint-artwork/sounds/unplug.oga'
+#
+gsettings set org.cinnamon.sounds switch-file '/usr/share/mint-artwork/sounds/unplug.oga'
+#
+gsettings set org.cinnamon.sounds tile-file '/usr/share/mint-artwork/sounds/unplug.oga'
+#
+gsettings set org.cinnamon.sounds notification-file '/usr/share/mint-artwork/sounds/unplug.oga'
+#
+gsettings set org.cinnamon.sounds minimize-file '/usr/share/mint-artwork/sounds/unplug.oga'
+#
+gsettings set org.cinnamon.sounds unmaximize-file '/usr/share/mint-artwork/sounds/unplug.oga'
+
